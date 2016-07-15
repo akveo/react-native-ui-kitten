9e2000cc8afc3d5dfc842d5578c7c360415c5216
alex <demeshko.alexander.v@gmail.com>
2016-07-15 12:17:36 +0300

    refactor(bar bg): hide on landscape

--- components/status/BarBg.js
+++ components/status/BarBg.js
@@ -2,17 +2,22 @@ import React, {Component} from 'react';
 
 import {
   View,
+  Dimensions
 } from 'react-native';
 
 import { RkConfig} from '../../config/config';
 
+
 export class RkBarBg extends Component {
 
   render() {
+    let {height, width} = Dimensions.get('window');
+    if(width > height) return null;
     return (
-      <View style={[{backgroundColor: RkConfig.colors.primary, height: 20, position: 'absolute', top: 0, left:0, right: 0}, this.props.style]}></View>
+      <View
+        style={[{backgroundColor: RkConfig.colors.primary, height: 20, position: 'absolute', top: 0, left:0, right: 0}, this.props.style]}>
+      </View>
     );
   }
-
 
 }
