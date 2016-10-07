dfd2629f4f3918ba81b8002e125b1ea7c1ad9840
alex <demeshko.alexander.v@gmail.com>
2016-10-07 19:11:27 +0300

    refactor(exampleBoom): make settings screen font bigger

--- examples/BoomApp/app/screens/settings/SettingsScreenClassic.js
+++ examples/BoomApp/app/screens/settings/SettingsScreenClassic.js
@@ -19,7 +19,7 @@ export default class SettingsScreenClassic extends Component {
 
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       authIndex: ScreenService.getCurrentLoginScreenIndex()
     }
   }
@@ -31,40 +31,40 @@ export default class SettingsScreenClassic extends Component {
         style={[RkStyle.lightGrayBg, {paddingTop: 20}]}
         automaticallyAdjustContentInsets={true}>
         <Text style={styles.settingsTitle}>LOGIN/REGISTER SCREENS</Text>
-        <View style={styles.settingSection}>
+        <View style={styles.settingsSection}>
           <RkChoiceGroup
             radio
             type='clear'
             selectedIndex={this.state.authIndex}
             onChange={(index)=> this._changeScreen(index, 'authIndex', 'setLoginScreen')}>
             <TouchableOpacity choiceTrigger>
-              <View style={styles.screenContainer}>
-                <Text>Classic</Text>
-                <RkChoice innerStyle={{fontSize: 14, marginTop: 5}}/>
+              <View style={styles.setting}>
+                <Text style={styles.settingLabel}>Classic</Text>
+                <RkChoice/>
               </View>
             </TouchableOpacity>
             <TouchableOpacity choiceTrigger>
-              <View style={styles.screenContainer}>
-                <Text>Material</Text>
-                <RkChoice innerStyle={{fontSize: 14, marginTop: 10}}/>
+              <View style={styles.setting}>
+                <Text style={styles.settingLabel}>Material</Text>
+                <RkChoice/>
               </View>
             </TouchableOpacity>
             <TouchableOpacity choiceTrigger>
-              <View style={[styles.screenContainer, {borderBottomWidth: 0}]}>
-                <Text>Blur</Text>
-                <RkChoice innerStyle={{fontSize: 14, marginTop: 10}}/>
+              <View style={[styles.setting, styles.noBottomBorder]}>
+                <Text style={styles.settingLabel}>Blur</Text>
+                <RkChoice/>
               </View>
             </TouchableOpacity>
           </RkChoiceGroup>
         </View>
-        <View style={styles.settingSection}>
-          <RkButton type='clear small' onPress={()=>this._logOut()}>LOG OUT</RkButton>
+        <View style={[styles.settingsSection, styles.settingsSectionSymmetric]}>
+            <RkButton type='clear' onPress={()=>this._logOut()}>LOG OUT</RkButton>
         </View>
       </ScrollView>
     );
   }
 
-  _changeScreen(index, stateName, screenName){
+  _changeScreen(index, stateName, screenName) {
     let newState = {};
     newState[stateName] = index;
     this.setState(newState);
@@ -72,7 +72,7 @@ export default class SettingsScreenClassic extends Component {
   }
 
 
-  _logOut(){
+  _logOut() {
     this.props.navigator.push({
       component: ScreenService.getLoginScreen(true),
     });
@@ -82,32 +82,44 @@ export default class SettingsScreenClassic extends Component {
 
 let styles = StyleSheet.create({
 
-  settingSection: {
-    marginBottom: 20,
+  settingsSection: {
+    marginBottom: 30,
     backgroundColor: 'white',
-    paddingLeft: 10,
-    paddingVertical: 5,
+    paddingLeft: 15,
     borderTopWidth: 0.5,
     borderBottomWidth: 0.5,
     borderTopColor: RkConfig.colors.gray,
     borderBottomColor: RkConfig.colors.gray,
   },
 
+  settingsSectionSymmetric: {
+    paddingLeft: 0
+  },
+
   settingsTitle: {
     paddingHorizontal: 10,
     paddingVertical: 5,
-    fontSize: 12,
+    fontSize: 13,
     color: RkConfig.colors.darkGray
   },
 
-  screenContainer: {
+  setting: {
     "flexDirection": "row",
     justifyContent: "space-between",
     "alignItems": "center",
-    paddingVertical: 5,
-    paddingRight: 5,
+    paddingVertical: 10,
+    paddingRight: 10,
     borderBottomWidth: 0.5,
     borderBottomColor: RkConfig.colors.gray,
+  },
+
+  settingLabel: {
+    fontSize: 18
+  },
+
+  noBottomBorder: {
+    borderBottomWidth: 0
   }
+
 
 });
