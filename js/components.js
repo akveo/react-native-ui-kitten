3f4e5fd5ab9e7dc0595786c06b3cb175521fe890
alex <demeshko.alexander.v@gmail.com>
2016-12-20 12:09:49 +0300

    Updates

---
+++ js/components.js
@@ -0,0 +1,27 @@
+document.addEventListener( "DOMContentLoaded", function(){
+  var phone = document.getElementById('phoneImg');
+  var phoneOffset = document.getElementById('phone').offsetTop;
+  var components = document.getElementsByClassName('component');
+  var currentImageComponent = -1;
+  function selectImageComponent(scrolled) {
+    scrolled = scrolled + phoneOffset;
+    for(var i = 0; i < components.length; i++){
+      var condition = scrolled > components[i].offsetTop;
+      if(components[i+1]) condition = condition && components[i+1].offsetTop > scrolled;
+      if(condition){
+        if(currentImageComponent === i) return;
+        currentImageComponent = i;
+        phone.src = '/images/components/' + components[i].getAttribute('image');
+        return;
+      }
+    }
+  }
+
+  selectImageComponent(window.pageYOffset || document.documentElement.scrollTop);
+
+  window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    selectImageComponent(scrolled);
+  };
+
+}, false );
