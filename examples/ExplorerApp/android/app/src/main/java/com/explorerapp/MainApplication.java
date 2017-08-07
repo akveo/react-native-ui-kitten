bc533b11784f432dc56b37f760a98ba1cae024fb
KovalM <michail.kovalev217@gmail.com>
2017-08-07 16:30:09 +0300

    build(common): update react-native to 0.47

--- examples/ExplorerApp/android/app/src/main/java/com/explorerapp/MainApplication.java
+++ examples/ExplorerApp/android/app/src/main/java/com/explorerapp/MainApplication.java
@@ -1,16 +1,12 @@
 package com.explorerapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
