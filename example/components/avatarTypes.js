9b3347ee0b3d23655ab7a6a88012be31298d55cf
Artur Yorsh <artyorsh.me@gmail.com>
2018-06-14 17:53:18 +0300

    refactor(lint): fix code style warnings (avatarTypes)

--- example/components/avatarTypes.js
+++ example/components/avatarTypes.js
@@ -1,38 +1,36 @@
-export const AvatarTypes = (theme) => {
-  return ({
-    _base: {
-      container: {
-        flex: 1,
-        alignItems: 'center',
-        flexDirection: 'row',
-        marginVertical: 4
-      },
-      image: {
-        width: 40,
-        height: 40
-      },
-      username: {
-        paddingLeft: 10,
-        color: theme.colors.text.base
-      },
-      description: {
-        paddingLeft: 10,
-        color: theme.colors.text.hint,
-        fontSize: theme.fonts.sizes.small
-      },
+export const AvatarTypes = (theme) => ({
+  _base: {
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      flexDirection: 'row',
+      marginVertical: 4,
     },
-    round: {
-      image: {
-        borderRadius: 20,
-        width:36,
-        height:36,
-        margin:2
-      }
+    image: {
+      width: 40,
+      height: 40,
     },
-    info: {
-      backgroundColor: theme.colors.screen.info,
-      color: theme.colors.text.inverse,
-      descriptionColor: theme.colors.text.inverse
-    }
-  })
-};
+    username: {
+      paddingLeft: 10,
+      color: theme.colors.text.base,
+    },
+    description: {
+      paddingLeft: 10,
+      color: theme.colors.text.hint,
+      fontSize: theme.fonts.sizes.small,
+    },
+  },
+  round: {
+    image: {
+      borderRadius: 20,
+      width: 36,
+      height: 36,
+      margin: 2,
+    },
+  },
+  info: {
+    backgroundColor: theme.colors.screen.info,
+    color: theme.colors.text.inverse,
+    descriptionColor: theme.colors.text.inverse,
+  },
+});
