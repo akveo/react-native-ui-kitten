0c68e5ac66f6ef23d50fabb9afe843f939afde37
Artur Yorsh <artyorsh.me@gmail.com>
2018-07-12 19:27:45 +0300

    refactor(rkGallery): default component styles

---
+++ src/components/gallery/types.js
@@ -0,0 +1,17 @@
+export const RkGalleryTypes = (theme) => ({
+  _base: {
+    grid: {
+      backgroundColor: theme.colors.transparent,
+    },
+    gridItem: {
+      margin: 2,
+    },
+    previewHeader: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      padding: 16,
+    },
+    previewFooter: {},
+  },
+});
