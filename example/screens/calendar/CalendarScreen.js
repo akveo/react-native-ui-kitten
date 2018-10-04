55338c216829c03b92c43c0d218e9a6bccf2b35d
Artur Yorsh <artyorsh.me@gmail.com>
2018-10-04 17:00:31 +0300

    feat(example): add calendar examples description

--- example/screens/calendar/CalendarScreen.js
+++ example/screens/calendar/CalendarScreen.js
@@ -11,6 +11,9 @@ import {
 } from 'react-native-ui-kitten';
 
 export class CalendarScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Calendars',
+  };
   static propTypes = {
     navigation: PropTypes.shape({
       navigate: PropTypes.func.isRequired,
@@ -19,22 +22,27 @@ export class CalendarScreen extends React.Component {
   static data = [
     {
       title: 'Base',
+      description: "'Minimum code' implementation",
       route: 'BaseCalendar',
     },
     {
       title: 'Range',
+      description: 'If you want to select date range',
       route: 'RangeCalendar',
     },
     {
       title: 'Bounding Month',
+      description: 'No next month days at the end of the current',
       route: 'BoundingCalendar',
     },
     {
       title: 'Custom Day Cell',
+      description: "Don't like default implementation?",
       route: 'CustomCalendar',
     },
     {
       title: 'Horizontal',
+      description: 'Like vertical but horizontal',
       route: 'HorizontalCalendar',
     },
   ];
@@ -48,19 +56,26 @@ export class CalendarScreen extends React.Component {
   renderItem = ({ item }) => (
     <TouchableOpacity onPress={() => this.onItemClick(item)}>
       <View style={styles.componentRow}>
-        <RkText rkType='bold'>{item.title}</RkText>
+        <RkText rkType='header'>{item.title}</RkText>
+        <RkText rkType='subtitle'>{item.description}</RkText>
       </View>
     </TouchableOpacity>
   );
 
   render() {
     return (
-      <FlatList
-        style={styles.container}
-        data={CalendarScreen.data}
-        keyExtractor={this.getItemKey}
-        renderItem={this.renderItem}
-      />
+      <View style={styles.container}>
+        <RkText>
+          {'RkCalendar is a component which allows user to select date or date range.' +
+          ' Check out some examples below prepared for you to see the power!'}
+        </RkText>
+        <View style={styles.descriptionSeparator} />
+        <FlatList
+          data={CalendarScreen.data}
+          keyExtractor={this.getItemKey}
+          renderItem={this.renderItem}
+        />
+      </View>
     );
   }
 }
@@ -68,13 +83,17 @@ export class CalendarScreen extends React.Component {
 const styles = RkStyleSheet.create(theme => ({
   container: {
     flex: 1,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
     backgroundColor: theme.colors.screen.base,
   },
+  descriptionSeparator: {
+    height: 2,
+    marginVertical: 8,
+    backgroundColor: theme.colors.border.base,
+  },
   componentRow: {
-    paddingHorizontal: 24,
-    paddingVertical: 18,
-    flexDirection: 'row',
+    paddingVertical: 8,
     justifyContent: 'space-between',
-    alignItems: 'center',
   },
 }));
