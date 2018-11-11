3a87286d39adb41c8987ffca034f0e27b9558f2b
Artur Yorsh <artyorsh.me@gmail.com>
2018-11-11 19:33:36 +0300

    chore(playground): remove themed component example

--- src/playground/src/App.component.tsx
+++ src/playground/src/App.component.tsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import { Sample } from '@rk-kit/ui';
-import {
-  ThemeProvider,
-  withTheme,
-} from '@rk-kit/theme';
-
-class ThemedSample extends React.Component<any> {
-
-  render() {
-    return (
-      <Sample text={this.props.theme.text}/>
-    );
-  }
-}
 
 export default class App extends React.Component {
-
-  state = {
-    theme: {
-      text: 'test',
-    },
-  };
-
   render() {
-    const ThemedComponent = withTheme(ThemedSample);
     return (
-      <ThemeProvider theme={this.state.theme}>
-        <ThemedComponent/>
-      </ThemeProvider>
+      <Sample/>
     );
   }
 }
