b6518b1802345a20c88d5ce32dc39bf5e5e82ed0
Artur Yorsh <artyorsh.me@gmail.com>
2018-11-12 20:19:26 +0300

    test(theme): add theme change test

--- src/framework/theme/theme.spec.tsx
+++ src/framework/theme/theme.spec.tsx
@@ -1,19 +1,73 @@
 import React from 'react';
-import { View } from 'react-native';
-import { render } from 'react-native-testing-library';
+import {
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  waitForElement,
+  fireEvent,
+  render,
+} from 'react-native-testing-library';
 import {
   ThemeProvider,
   withTheme,
   withThemedStyles,
-  ThemeShape,
+  ThemeType,
 } from './component';
 
 const themeConsumerTestId = '@theme/consumer';
+const themeChangeTouchableTestId = '@theme/btnChangeTheme';
 
-class TestComponent extends React.Component<any> {
+class ConsumerComponent extends React.Component<any> {
+  static defaultProps = {
+    testID: themeConsumerTestId,
+  };
+
   render() {
     return (
-      <View testID={this.props.testId}/>
+      <View testID={this.props.testID}/>
+    );
+  }
+}
+
+class ActionComponent extends React.Component<any> {
+
+  static initialThemeColor = '#ffffff';
+  static onChangeThemeColor = '#000000';
+
+  state = {
+    theme: {
+      backgroundColor: ActionComponent.initialThemeColor,
+    },
+  };
+
+  isInitialColor = (color: string): boolean => color === ActionComponent.initialThemeColor;
+
+  getInversedColor = (color: string): string => {
+    const isInitialColor = this.isInitialColor(color);
+    return isInitialColor ? ActionComponent.onChangeThemeColor : ActionComponent.initialThemeColor;
+  };
+
+  onThemeChangeTouchablePress = () => {
+    this.setState({
+      theme: {
+        backgroundColor: this.getInversedColor(this.state.theme.backgroundColor),
+      },
+    });
+  };
+
+  render() {
+    const ThemedComponent = withTheme(ConsumerComponent);
+    return (
+      <View>
+        <ThemeProvider theme={this.state.theme}>
+          <ThemedComponent/>
+        </ThemeProvider>
+        <TouchableOpacity
+          testID={themeChangeTouchableTestId}
+          onPress={this.onThemeChangeTouchablePress}
+        />
+      </View>
     );
   }
 }
@@ -21,11 +75,11 @@ class TestComponent extends React.Component<any> {
 describe('@theme: theme consumer checks', () => {
 
   it('renders properly', async () => {
-    const ThemedComponent = withTheme(TestComponent);
+    const ThemedComponent = withTheme(ConsumerComponent);
 
     const component = render(
       <ThemeProvider theme={{}}>
-        <ThemedComponent testId={themeConsumerTestId}/>
+        <ThemedComponent/>
       </ThemeProvider>,
     );
 
@@ -34,11 +88,11 @@ describe('@theme: theme consumer checks', () => {
   });
 
   it('receives theme prop', async () => {
-    const ThemedComponent = withTheme(TestComponent);
+    const ThemedComponent = withTheme(ConsumerComponent);
 
     const component = render(
       <ThemeProvider theme={{}}>
-        <ThemedComponent testId={themeConsumerTestId}/>
+        <ThemedComponent/>
       </ThemeProvider>,
     );
 
@@ -46,18 +100,34 @@ describe('@theme: theme consumer checks', () => {
     expect(themedComponent.props.theme).not.toBeNull();
   });
 
+  it('receives theme prop on theme change', async () => {
+    const component = render(
+      <ActionComponent/>,
+    );
+
+    const touchableComponent = component.getByTestId(themeChangeTouchableTestId);
+
+    fireEvent.press(touchableComponent);
+
+    const themedComponent = await waitForElement(() => {
+      return component.getByTestId(themeConsumerTestId);
+    });
+
+    expect(themedComponent.props.theme.backgroundColor).toEqual(ActionComponent.onChangeThemeColor);
+  });
+
 });
 
 describe('@theme: styled theme consumer checks', () => {
 
   it('renders properly', async () => {
-    const ThemedComponent = withThemedStyles(TestComponent, (theme: ThemeShape) => {
+    const ThemedComponent = withThemedStyles(ConsumerComponent, (theme: ThemeType) => {
       return {};
     });
 
     const component = render(
       <ThemeProvider theme={{}}>
-        <ThemedComponent testId={themeConsumerTestId}/>
+        <ThemedComponent testID={themeConsumerTestId}/>
       </ThemeProvider>,
     );
 
@@ -66,13 +136,13 @@ describe('@theme: styled theme consumer checks', () => {
   });
 
   it('receives theme prop', async () => {
-    const ThemedComponent = withThemedStyles(TestComponent, (theme: ThemeShape) => {
+    const ThemedComponent = withThemedStyles(ConsumerComponent, (theme: ThemeType) => {
       return {};
     });
 
     const component = render(
       <ThemeProvider theme={{}}>
-        <ThemedComponent testId={themeConsumerTestId}/>
+        <ThemedComponent/>
       </ThemeProvider>,
     );
 
@@ -81,13 +151,13 @@ describe('@theme: styled theme consumer checks', () => {
   });
 
   it('receives themedStyle prop', async () => {
-    const ThemedComponent = withThemedStyles(TestComponent, (theme: ThemeShape) => {
+    const ThemedComponent = withThemedStyles(ConsumerComponent, (theme: ThemeType) => {
       return {};
     });
 
     const component = render(
       <ThemeProvider theme={{}}>
-        <ThemedComponent testId={themeConsumerTestId}/>
+        <ThemedComponent/>
       </ThemeProvider>,
     );
 
