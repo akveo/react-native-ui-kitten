439104fe76dc23c39c2de5a10b1ffbf4c84e656b
Artur Yorsh <artyorsh.me@gmail.com>
2018-11-27 18:52:36 +0300

    feat(design): initial design tool integration

---
+++ src/framework/design/config/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Mocked component design configurations
+ */
+
+import { config as Components } from './components.json';
+import { config as Tokens } from './tokens.json';
+
+export {
+  Components,
+  Tokens,
+};
