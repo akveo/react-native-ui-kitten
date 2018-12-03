ac71527cb1f5c3558f0c3f5107a60c975bb16b58
Artur Yorsh <artyorsh.me@gmail.com>
2018-12-03 09:54:15 +0300

    feat(theme): create style from list of variants

--- src/framework/theme/service/themeUtil.service.ts
+++ src/framework/theme/service/themeUtil.service.ts
@@ -1,9 +1,8 @@
-import {
-  DesignType,
-  MappingType,
-} from '@rk-kit/design';
+import { DesignType, MappingType } from '@rk-kit/design';
 import { getComponentMapping } from './designUtil.service';
 import { ThemeType } from '../../theme';
+
+const variantSeparator = ' ';
 
 /**
  * Creates flat style object which can be used to create StyleSheet styles.
@@ -13,23 +12,28 @@ import { ThemeType } from '../../theme';
  * @param variant: string - variant name. Default is 'default'
  *
  * @return any.
- *
- * TODO(theme/service): handle variations like `dark success`
  */
+
+// TODO(theme/@type): declare Style type
+type StyleType = any;
+
 export function createFlatStyle(theme: ThemeType,
                                 design: DesignType,
-                                variant: string = 'default'): any {
+                                variant: string = 'default'): StyleType {
 
-  const defaultMapping = getComponentMapping(design, 'default');
-  const variantMapping = getComponentMapping(design, variant);
+  const mapVariant = (v: string) => createStyleFromVariant(theme, design, v);
+  const mergeStyles = (origin: StyleType, next: StyleType) => ({ ...origin, ...next });
 
-  return {
-    ...createStyleFromMapping(defaultMapping, theme),
-    ...createStyleFromMapping(variantMapping, theme),
-  };
+  const defaultStyle = createStyleFromVariant(theme, design, 'default');
+  return variant.split(variantSeparator).map(mapVariant).reduce(mergeStyles, defaultStyle);
 }
 
-function createStyleFromMapping(mapping: MappingType[], theme: ThemeType): any {
+function createStyleFromVariant(theme: ThemeType, design: DesignType, variant: string): StyleType {
+  const variantMapping = getComponentMapping(design, variant);
+  return createStyleFromMapping(variantMapping, theme);
+}
+
+function createStyleFromMapping(mapping: MappingType[], theme: ThemeType): StyleType {
   const assignParameter = (style: any, prop: MappingType) => {
     style[prop.parameter] = theme.find(value => value.name === prop.token).value;
     return style;
