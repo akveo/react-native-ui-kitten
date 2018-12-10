c615a2d36a04c250406c2e3a33fcda1fbe9eaa30
Artur Yorsh <artyorsh.me@gmail.com>
2018-12-10 19:32:42 +0300

    refactor(theme): remove unused exports

--- src/framework/theme/index.ts
+++ src/framework/theme/index.ts
@@ -1,2 +1,26 @@
-export * from './component';
-export * from './service';
+export {
+  StyleProvider,
+  StyledComponent,
+  ThemeProvider,
+  withStyles,
+
+  StyleProviderProps,
+  StyledComponentProps,
+  ThemeProviderProps,
+  ThemedComponentProps,
+
+  ThemeType,
+  ThemeMappingType,
+  ParameterType,
+  VariantType,
+  MappingType,
+  TokenType,
+  ThemedStyleType,
+  StyleSheetType,
+  StyleType,
+} from './component';
+
+export {
+  VARIANT_DEFAULT,
+} from './service';
+
