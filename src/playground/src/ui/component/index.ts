2376abeee9fc22ea6d2ad703ac5fd5cd35e0d915
Artur Yorsh <artyorsh.me@gmail.com>
2018-12-22 17:56:23 +0300

    refactor(playground): remove sample component

--- src/playground/src/ui/component/index.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from '@rk-kit/theme';
-import { Sample, Props } from './sample.component';
-
-const StyledSample = styled<Sample, Props>(Sample);
-
-export {
-  StyledSample as Sample,
-  Props as SampleProps,
-};
--- src/playground/src/ui/screen/index.ts
+++ src/playground/src/ui/screen/index.ts
@@ -1,3 +1,2 @@
 export { HomeScreen } from './home.component';
 export { RadioScreen } from './radio.component';
-export { SampleScreen } from './sample.component';
