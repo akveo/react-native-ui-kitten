64010c49ca89a77168df449ea7866208ac9ca94a
Artur Yorsh <artyorsh.me@gmail.com>
2019-02-08 16:22:02 +0300

    fix(theme): nested style mapping

--- src/framework/theme/service/theme/theme.service.ts
+++ src/framework/theme/service/theme/theme.service.ts
@@ -3,9 +3,10 @@ import { ThemeType } from '../../type';
 /**
  * @param name: string - theme property name, like `backgroundColor`
  * @param theme: ThemeType - theme
+ * @param fallback: any - fallback value
  *
- * @return any. Theme property value if it presents in theme, undefined otherwise
+ * @return any. Theme property value if it presents in theme, fallback otherwise
  */
-export function getThemeValue(name: string, theme: ThemeType): any | undefined {
-  return theme[name];
+export function getThemeValue(name: string, theme: ThemeType, fallback?: any): any | undefined {
+  return theme[name] || fallback;
 }
