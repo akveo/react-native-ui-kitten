c0af7a70889134e845dc74295622876ef129ec2f
Yauhen Penkin <e.penkin@akveo.com>
2019-03-21 14:52:05 +0300

    docs(ui): checkbox component docs add

--- src/framework/ui/checkbox/checkbox.component.tsx
+++ src/framework/ui/checkbox/checkbox.component.tsx
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
 import React from 'react';
 import {
   View,
@@ -18,11 +24,59 @@ import {
 } from '../text/text.component';
 import { CheckMark } from '../drawable/checkmark/checkmark.component';
 
+/**
+ * The `Checkbox` component is an analog of html checkbox button.
+ *
+ * @extends React.Component
+ *
+ * @property {boolean} checked - Determines whether component is checked.
+ *
+ * @property {boolean} disabled - Determines whether component is disabled.
+ * By default is false.
+ *
+ * @property {string} status - Determines the status of the component.
+ * Can be 'primary' | 'success' | 'info' | 'warning' | 'danger'.
+ * By default status is 'primary'.
+ *
+ * @property {(checked: boolean) => void} onChange - Triggered on change value.
+ *
+ * @example Simple usage example
+ *
+ * ```tsx
+ * import { Toggle } from '@kitten/ui';
+ * <Checkbox checked={true}/>
+ * ```
+ *
+ * ### Usage
+ *
+ * @example
+ *
+ * ```tsx
+ * import { Checkbox } from '@kitten/ui';
+ *
+ * state: State = {
+ *   checked: false,
+ * };
+ *
+ * private onChange = (checked: boolean): void => {
+ *   this.setState({ checked: checked });
+ * };
+ *
+ * public render(): React.ReactNode {
+ *   return (
+ *     <Checkbox
+ *       checked={this.state.toggled}
+ *       status='info'
+ *       onChange={this.onChange}/>
+ *   );
+ * }
+ * ```
+ * */
+
 interface CheckBoxProps {
   text?: string;
   checked?: boolean;
   status?: string;
-  size?: string;
   onChange?: (checked: boolean) => void;
 }
 
