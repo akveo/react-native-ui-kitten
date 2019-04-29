c2620ffa80ddd44ce5c32f5f4af1d154ae3b8c10
Yauhen Penkin <e.penkin@akveo.com>
2019-04-29 11:23:37 +0300

    fix(ui): return required constant for extracting styles (temporary)

--- src/framework/ui/common/props.ts
+++ src/framework/ui/common/props.ts
@@ -1,3 +1,35 @@
+export const TextStyleProps: string[] = [
+
+  // TextStyle
+
+  'color',
+  'fontFamily',
+  'fontSize',
+  'fontStyle',
+  'fontWeight',
+  'letterSpacing',
+  'lineHeight',
+  'textAlign',
+  'textAlign',
+  'textDecorationLine',
+  'textDecorationStyle',
+  'textDecorationColor',
+  'textShadowColor',
+  'textShadowColor',
+  'textShadowOffset',
+  'textShadowRadius',
+
+  // TextStyleIOS
+
+  'textTransform',
+  'writingDirection',
+
+  // TextStyleAndroid
+
+  'textAlignVertical',
+  'includeFontPadding',
+];
+
 export const FlexStyleProps: string[] = [
   'alignContent',
   'alignItems',
