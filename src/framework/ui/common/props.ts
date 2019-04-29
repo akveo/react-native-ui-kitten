f11d03eb776d091f8daf3fde13fea508440cfc7e
Yauhen Penkin <e.penkin@akveo.com>
2019-04-29 11:15:58 +0300

    feat(ui): button "textStyle" property add

--- src/framework/ui/common/props.ts
+++ src/framework/ui/common/props.ts
@@ -1,35 +1,3 @@
-export const TextStyleProps: string[] = [
-
-  // TextStyle
-
-  'color',
-  'fontFamily',
-  'fontSize',
-  'fontStyle',
-  'fontWeight',
-  'letterSpacing',
-  'lineHeight',
-  'textAlign',
-  'textAlign',
-  'textDecorationLine',
-  'textDecorationStyle',
-  'textDecorationColor',
-  'textShadowColor',
-  'textShadowColor',
-  'textShadowOffset',
-  'textShadowRadius',
-
-  // TextStyleIOS
-
-  'textTransform',
-  'writingDirection',
-
-  // TextStyleAndroid
-
-  'textAlignVertical',
-  'includeFontPadding',
-];
-
 export const FlexStyleProps: string[] = [
   'alignContent',
   'alignItems',
