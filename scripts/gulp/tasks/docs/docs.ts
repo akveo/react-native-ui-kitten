d568d0e4931f0b38ae1c50a14aa772ce5c4f7373
Yauhen Penkin <e.penkin@akveo.com>
2019-05-03 18:02:31 +0300

    feat(docs): generating docs scripts add

--- scripts/gulp/tasks/docs/docs.ts
+++ scripts/gulp/tasks/docs/docs.ts
@@ -3,32 +3,15 @@ import {
   task,
 } from 'gulp';
 import './example';
-// import { structure as DOCS } from '../../../../docs/structure';
-import {
-  existsSync,
-  mkdirSync,
-  readFileSync,
-  writeFileSync,
-} from 'fs';
-import {
-  isAbsolute,
-  join,
-  resolve,
-  sep,
-} from 'path';
-import { DOCS_DIST } from '../config';
 
 const typedoc = require('gulp-typedoc');
 const exec = require('child_process').execSync;
 
-task('docs', ['generate-doc-json']);
 task('generate-doc-json', generateDocJson);
+
 task('process-type-doc', ['generate-doc-json'], processTypeDoc);
 
-// task('create-docs-dirs', () => {
-//   const docsStructure = flatten('docs', routesTree(DOCS));
-//   createDirsStructure(docsStructure);
-// });
+task('docs', ['generate-doc-json', 'process-type-doc']);
 
 function generateDocJson() {
   return src(['src/framework/**/*.tsx', '!src/framework/**/*.spec.tsx'])
@@ -38,7 +21,6 @@ function generateDocJson() {
       resolveJsonModule: true,
       moduleResolution: 'node',
       jsx: 'react',
-      tsx: 'react',
       target: 'ES6',
       module: 'commonjs',
       baseUrl: './',
@@ -52,76 +34,5 @@ function generateDocJson() {
 }
 
 function processTypeDoc() {
-  return exec('prsr -g typedoc -f angular -i docs/docs.json -o docs/output.json');
+  return exec('prsr -g typedoc -f react -i docs/docs.json -o docs/src/input.json');
 }
-
-// function routesTree(structure) {
-//   return structure
-//     .filter((page: any) => ['section', 'page', 'tabs'].includes(page.type))
-//     .map((page: any) => {
-//       if (page.type === 'tabs') {
-//         page.children = ['overview', 'api', 'theme', 'examples']
-//           .map(name => ({
-//             name,
-//             type: 'page',
-//           }));
-//       }
-//       return page;
-//     })
-//     .map((page: any) => {
-//       return {
-//         path: prepareSlag(page.name),
-//         children: page.children ? routesTree(page.children) : [],
-//       };
-//     });
-// }
-
-// function prepareSlag(name) {
-//   return name.replace(/[^a-zA-Z0-9\s]+/g, '')
-//              .replace(/\s/g, '-')
-//              .toLowerCase();
-// }
-//
-// function flatten(root, arr) {
-//   let res: any[] = [];
-//   arr.forEach((item: any) => {
-//     const path = `${root}/${item.path}`;
-//     res.push(path);
-//     if (item.children) {
-//       res = res.concat(flatten(path, item.children));
-//     }
-//   });
-//
-//   return res;
-// }
-//
-// function createDirsStructure(dirs) {
-//   const index = readFileSync(join(DOCS_DIST, 'index.html'), 'utf8');
-//   dirs.forEach((dir: any) => {
-//     const fullPath = join(DOCS_DIST, dir);
-//     if (!existsSync(fullPath)) {
-//       mkDirByPathSync(fullPath);
-//     }
-//
-//     writeFileSync(join(fullPath, 'index.html'), index);
-//   });
-// }
-//
-// function mkDirByPathSync(targetDir, { isRelativeToScript = false } = {}) {
-//   const initDir = isAbsolute(targetDir) ? sep : '';
-//   const baseDir = isRelativeToScript ? __dirname : '.';
-//
-//   targetDir.split(sep)
-//            .reduce((parentDir, childDir) => {
-//              const curDir = resolve(baseDir, parentDir, childDir);
-//              try {
-//                mkdirSync(curDir);
-//              } catch (err) {
-//                if (err.code !== 'EEXIST') {
-//                  throw err;
-//                }
-//              }
-//
-//              return curDir;
-//            }, initDir);
-// }
