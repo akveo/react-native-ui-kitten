bfbf259ad5bf19f1b3fd24c3819c05e64bb7ede9
Artur Yorsh <artyorsh.me@gmail.com>
2019-05-17 00:46:40 +0300

    build(babel): refactor package.json usage with babel aliases

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,28 @@
-module.exports = {
-  presets: [
+const path = require('path');
+
+const frameworkModulePath = path.resolve('./src/framework');
+
+// FIXME: Resolve `transform[stderr]: Could not resolve` command-line warnings.
+// FIXME: Reproducible when starting with clearing cache (npm start -- -c)
+
+const moduleResolverConfig = {
+  root: path.resolve('./'),
+  alias: {
+    '@kitten/theme': path.resolve(frameworkModulePath, 'theme'),
+    '@kitten/ui': path.resolve(frameworkModulePath, 'ui'),
+  },
+};
+
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
     'module:metro-react-native-babel-preset',
-  ]
+  ];
+
+  const plugins = [
+    ['module-resolver', moduleResolverConfig],
+  ];
+
+  return { presets, plugins };
 };
