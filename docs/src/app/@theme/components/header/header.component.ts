22bf386ca0df1363f6cd9b000ec2cc1567007827
malashkevich <alexei.malashkevich@gmail.com>
2019-06-17 14:28:25 +0300

    fix(docs): add closing tag for search component

--- docs/src/app/@theme/components/header/header.component.ts
+++ docs/src/app/@theme/components/header/header.component.ts
@@ -17,7 +17,7 @@ import { NgdVersionService } from '../../services';
     </div>
     <div class="section middle">
       <nb-menu [items]="mainMenu"></nb-menu>
-      <ngd-search />
+      <ngd-search></ngd-search>
       <nb-select class="version-select" [selected]="currentVersion" (selectedChange)="redirectToVersion($event)">
         <nb-option *ngFor="let version of versions" [value]="version">
           {{ version }}
@@ -36,7 +36,6 @@ import { NgdVersionService } from '../../services';
 })
 export class NgdHeaderComponent implements OnInit {
 
-  @Input() showSearch = true;
   @HostBinding('class.docs-page') @Input() isDocs = false;
 
   private window: Window;
