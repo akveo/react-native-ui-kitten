0d0a7842bb8b4d4ad78a995169a7c1577732345e
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2019-06-21 16:17:06 +0300

    build: fix typescript build errors

--- src/framework/ui/support/typings/type.ts
+++ src/framework/ui/support/typings/type.ts
@@ -1,22 +1,22 @@
 import {
-  NativeSyntheticEvent,
-  NativeScrollEvent,
-  TextInputFocusEventData,
-  TextInputEndEditingEventData,
-  TouchableOpacityProps,
   GestureResponderEvent,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  TextInputEndEditingEventData,
+  TextInputFocusEventData,
+  TouchableOpacityProps,
 } from 'react-native';
 
-export type ScrollEvent = NativeSyntheticEvent<NativeScrollEvent>;
-export type InputFocusEvent = NativeSyntheticEvent<TextInputFocusEventData>;
-export type InputEndEditEvent = NativeSyntheticEvent<TextInputEndEditingEventData>;
+export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+export type Override<T, U> = Omit<T, keyof U> & U;
 
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-
-// @ts-ignore: props override
-export interface TouchableIndexedProps extends TouchableOpacityProps {
+export type TouchableIndexedProps = Override<TouchableOpacityProps, {
   onPress?: (index: number, event: GestureResponderEvent) => void;
   onPressIn?: (index: number, event: GestureResponderEvent) => void;
   onPressOut?: (index: number, event: GestureResponderEvent) => void;
   onLongPress?: (index: number, event: GestureResponderEvent) => void;
-}
+}>;
+
+export type ScrollEvent = NativeSyntheticEvent<NativeScrollEvent>;
+export type InputFocusEvent = NativeSyntheticEvent<TextInputFocusEventData>;
+export type InputEndEditEvent = NativeSyntheticEvent<TextInputEndEditingEventData>;
