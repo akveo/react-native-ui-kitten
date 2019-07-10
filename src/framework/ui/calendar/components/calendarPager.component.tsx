d274a28852222220bdf21913a836c106ac194aa5
Artur Yorsh <artyorsh.me@gmail.com>
2019-07-10 19:46:51 +0300

    feat(ui): calendar - increase paging performance

--- src/framework/ui/calendar/components/calendarPager.component.tsx
+++ src/framework/ui/calendar/components/calendarPager.component.tsx
@@ -15,12 +15,36 @@ export type CalendarPagerElement<D> = React.ReactElement<CalendarPagerProps<D>>;
 
 export class CalendarPager<D> extends React.Component<CalendarPagerProps<D>> {
 
+  private onSelect = (index: number) => {
+    // TODO: This fixes layout junks (for any reason)
+    setTimeout(() => {
+      this.props.onSelect(index);
+    });
+  };
+
+  private shouldLoadComponent = (index: number): boolean => {
+    if (this.props.shouldLoadComponent) {
+      return this.props.shouldLoadComponent(index);
+    }
+    return true;
+  };
+
+  private createChildElement = (date: D, index: number): React.ReactElement<any> => {
+    if (this.shouldLoadComponent(index)) {
+      return this.props.children(date, index);
+    }
+    return null;
+  };
+
   public render(): ViewPagerElement {
     const { data, children, ...restProps } = this.props;
 
     return (
-      <ViewPager {...restProps}>
-        {data.map(children)}
+      <ViewPager
+        {...restProps}
+        shouldLoadComponent={this.shouldLoadComponent}
+        onSelect={this.onSelect}>
+        {data.map(this.createChildElement)}
       </ViewPager>
     );
   }
