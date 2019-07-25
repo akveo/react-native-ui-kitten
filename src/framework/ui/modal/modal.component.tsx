84e57bf9505c985ebfc3e229c5def995b5b16161
Yauhen Penkin <e.penkin@akveo.com>
2019-07-25 16:34:05 +0300

    refactor(ui): modal lifecycle hooks refactor

--- src/framework/ui/modal/modal.component.tsx
+++ src/framework/ui/modal/modal.component.tsx
@@ -106,18 +106,18 @@ export class Modal extends React.Component<ModalProps> {
   private contentSize: Size = initialContentSize;
   private id: string = '';
 
-  public componentWillReceiveProps(nextProps: ModalProps): void {
-    if (this.props.visible !== nextProps.visible) {
-      this.handleVisibility(nextProps);
-    } else if (this.props.visible && nextProps.visible) {
-      ModalService.update(this.id, nextProps.children);
+  public componentDidUpdate(prevProps: ModalProps): void {
+    if (prevProps.visible !== this.props.visible) {
+      this.handleVisibility(this.props);
+    } else if (prevProps.visible && this.props.visible) {
+      ModalService.update(this.id, this.props.children);
     }
   }
 
-  private handleVisibility = (nextProps: ModalProps): void => {
+  private handleVisibility = (props: ModalProps): void => {
     const { allowBackdrop, onBackdropPress } = this.props;
 
-    if (nextProps.visible) {
+    if (props.visible) {
       const element: React.ReactElement = this.renderModal();
       this.id = ModalService.show(element, { allowBackdrop, onBackdropPress });
     } else {
