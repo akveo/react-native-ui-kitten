856b227e415806aac9c3c9abbb7b8f9b7dc1047d
Artur Yorsh <artyorsh.me@gmail.com>
2019-09-27 18:12:47 +0300

    release: 4.2.0

--- scripts/publish/publish-modules.sh
+++ scripts/publish/publish-modules.sh
@@ -4,7 +4,15 @@ env=${1}
 
 if [[ $env == "beta" ]]
 then
+    npm publish --tag beta --access=public ./dist/tsc-out/date-fns
     npm publish --tag beta --access=public ./dist/tsc-out/eva-icons
+    npm publish --tag beta --access=public ./dist/tsc-out/moment
+    npm publish --tag beta --access=public ./dist/tsc-out/templates/template-ui-kitten
+    npm publish --tag beta --access=public ./dist/tsc-out/templates/template-ui-kitten-typescript
 else
+    npm publish --access=public ./dist/tsc-out/date-fns
     npm publish --access=public ./dist/tsc-out/eva-icons
+    npm publish --access=public ./dist/tsc-out/moment
+    npm publish --access=public ./dist/tsc-out/templates/template-ui-kitten
+    npm publish --access=public ./dist/tsc-out/templates/template-ui-kitten-typescript
 fi
