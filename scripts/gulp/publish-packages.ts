ea44beec34721cb72ed12bcf9f8edaeacd17a6d1
Yauhen <e.penkin@akveo.com>
2019-12-03 20:16:18 +0300

    fix(ci): release scripts fix, docs-publishing script add

---
+++ scripts/gulp/publish-packages.ts
@@ -0,0 +1,36 @@
+import * as gulp from 'gulp';
+import { execSync } from 'child_process';
+import {
+  GulpCompletionCallback,
+  PACKAGES_BUILD_DIR,
+  PACKAGES_DIR,
+  ROOT_DIR,
+} from './common';
+
+gulp.task('publish-packages', gulp.series(
+  validate,
+  rebuild,
+  publish,
+));
+
+function validate(done: GulpCompletionCallback): void {
+  execSync('npm run lint', { cwd: ROOT_DIR });
+  execSync('npm run test', { cwd: ROOT_DIR });
+  done();
+}
+
+function rebuild(done: GulpCompletionCallback): void {
+  execSync('npm run clean', { cwd: ROOT_DIR });
+  execSync('npm run build', { cwd: ROOT_DIR });
+  done();
+}
+
+function publish(done: GulpCompletionCallback): void {
+  execSync(`npm publish ${PACKAGES_BUILD_DIR}/components`, { cwd: ROOT_DIR });
+  execSync(`npm publish ${PACKAGES_BUILD_DIR}/eva-icons`, { cwd: ROOT_DIR });
+  execSync(`npm publish ${PACKAGES_BUILD_DIR}/date-fns`, { cwd: ROOT_DIR });
+  execSync(`npm publish ${PACKAGES_BUILD_DIR}/components`, { cwd: ROOT_DIR });
+  execSync(`npm publish ${PACKAGES_DIR}/template-js`, { cwd: ROOT_DIR });
+  execSync(`npm publish ${PACKAGES_DIR}/template-ts`, { cwd: ROOT_DIR });
+  done();
+}
--- scripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-npm publish ./dist/tsc-out/components
-npm publish ./dist/tsc-out/date-fns
-npm publish ./dist/tsc-out/eva-icons
-npm publish ./dist/tsc-out/moment
-npm publish ./src/templates/template-js
-npm publish ./src/templates/template-ts
