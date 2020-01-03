97541fd9191507fa3f31deaf9a6c13f05c7691aa
Artur Yorsh <artyorsh.me@gmail.com>
2020-01-03 11:25:06 +0300

    Auto-generated commit

--- docs/assets/playground-build/service-worker.js
+++ docs/assets/playground-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4396cce4ebc7d33094ee001dd6195bd5.js"
+  "precache-manifest.780b7ca5a811dae35e96284c6e9f98c0.js"
 );
 
 workbox.clientsClaim();
