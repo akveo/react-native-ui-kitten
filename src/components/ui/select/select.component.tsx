231ba89841f1e85d793f2a9081fa5e8b5d309fd5
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2020-01-14 14:37:03 +0300

    fix(components): select - vertical scroll indicator

--- src/components/ui/select/select.component.tsx
+++ src/components/ui/select/select.component.tsx
@@ -408,7 +408,6 @@ class SelectComponent extends React.Component<SelectProps, State> {
 
     return (
       <SelectOptionsList
-        showsVerticalScrollIndicator={false}
         {...restProps}
         key={0}
         style={styles.optionsList}
