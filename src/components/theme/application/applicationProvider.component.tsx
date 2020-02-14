b7c3d822b07c7252f445ac69a1594d2cf8170184
Artur Yorsh <artyorsh.me@gmail.com>
2020-02-14 16:59:50 +0300

    feat(components): add styles property to ApplicationProvider

--- src/components/theme/application/applicationProvider.component.tsx
+++ src/components/theme/application/applicationProvider.component.tsx
@@ -7,11 +7,7 @@
 import React from 'react';
 import merge from 'lodash.merge';
 import { SchemaProcessor } from '@eva-design/processor';
-import {
-  CustomSchemaType,
-  SchemaType,
-  ThemeStyleType,
-} from '@eva-design/dss';
+import { CustomSchemaType, SchemaType, ThemeStyleType } from '@eva-design/dss';
 import { StyleProvider } from '../style/styleProvider.component';
 import { ThemeProviderProps } from '../theme/themeProvider.component';
 import { ModalPanel } from '../modal/modalPanel.component';
@@ -19,6 +15,7 @@ import { ModalPanel } from '../modal/modalPanel.component';
 interface ComponentProps {
   mapping: SchemaType;
   customMapping?: CustomSchemaType;
+  styles?: ThemeStyleType;
 }
 
 export type ApplicationProviderProps = ComponentProps & ThemeProviderProps;
@@ -38,13 +35,16 @@ interface State {
  * @extends React.Component
  *
  * @property {SchemaType} mapping - Determines the mapping for basic components.
- * This is designed to be provided by developers team and can be imported from npm package (e.g. `@eva-design/eva`).
+ * This is designed to be provided from any `@eva-design/*` package (e.g. `@eva-design/eva`)
  *
- * @property {CustomSchemaType} customMapping - Determines the customization mapping.
+ * @property {CustomSchemaType} customMapping - Determines the customized mapping.
  * This is merged with `mapping` property and designed to be used components customization.
  *
  * @property {ThemeType} theme - Determines the theme for basic components.
  * This is designed to be provided by developers team and can be imported from npm package (e.g. `@eva-design/eva`).
+ *
+ * @property {ThemeStyleType} styles - Determines the compiled styles.
+ * If ApplicationProvider receives this property, it will replace runtime mapping processing.
  *
  * @property {ReactNode} children - Determines application root component.
  *
@@ -73,20 +73,22 @@ interface State {
  */
 export class ApplicationProvider extends React.Component<ApplicationProviderProps, State> {
 
+  public state: State = {
+    styles: this.props.styles,
+  };
+
   private schemaProcessor: SchemaProcessor = new SchemaProcessor();
 
   constructor(props: ApplicationProviderProps) {
     super(props);
-    const { mapping, customMapping } = this.props;
 
-    const styles: ThemeStyleType = this.createStyles(mapping, customMapping);
-
-    this.state = { styles };
+    if (!this.state.styles) {
+      this.state.styles = this.createStyles(props.mapping, props.customMapping);
+    }
   }
 
   private createStyles = (mapping: SchemaType, custom: CustomSchemaType): ThemeStyleType => {
     const customizedMapping: SchemaType = merge({}, mapping, custom);
-
     return this.schemaProcessor.process(customizedMapping);
   };
 
