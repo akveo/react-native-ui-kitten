ca04bc52ff493f205c5c518010934e4050a19e5a
Artur Yorsh <artyorsh.me@gmail.com>
2020-02-17 21:58:25 +0300

    feat(metro-config): add custom mapping watcher if path was specified

--- src/metro-config/index.ts
+++ src/metro-config/index.ts
@@ -1,16 +1,21 @@
+import Fs from 'fs';
 import LodashMerge from 'lodash.merge';
 import MetroConfig from 'metro-config/src/defaults';
 import { EvaConfig } from './services/eva-config.service';
 import BootstrapService from './services/bootstrap.service';
+import ProjectService from './services/project.service';
 
 // TODO: TS definitions for metro config?
 type MetroConfigType = any;
 
 const defaultMetroConfig = MetroConfig.getDefaultValues();
+const customMappingWatchOptions = {
+  interval: 400,
+};
 
 export const create = (evaConfig: EvaConfig, projectConfig?: MetroConfigType): MetroConfigType => {
 
-  const uiKittenMetroConfig: MetroConfigType = {
+  const metroConfig: MetroConfigType = {
     reporter: {
       update: (event) => {
         const reporter = projectConfig.reporter || defaultMetroConfig.reporter;
@@ -19,12 +24,21 @@ export const create = (evaConfig: EvaConfig, projectConfig?: MetroConfigType): M
           reporter.update(event);
         }
 
-        if (event.type === 'bundle_build_started') {
+        if (event.type === 'initialize_started') {
           BootstrapService.run(evaConfig);
+
+          const customMappingPath = ProjectService.resolvePath(evaConfig.customMappingPath);
+          const customMappingExists: boolean = Fs.existsSync(customMappingPath);
+
+          if (customMappingExists) {
+            Fs.watchFile(customMappingPath, customMappingWatchOptions, () => {
+              BootstrapService.run(evaConfig);
+            });
+          }
         }
       },
     },
   };
 
-  return LodashMerge({}, projectConfig, uiKittenMetroConfig);
+  return LodashMerge({}, projectConfig, metroConfig);
 };
