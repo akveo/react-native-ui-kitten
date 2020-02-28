0f6ee761e1599312683761432ee6ba22fec51ea6
Artur Yorsh <artyorsh.me@gmail.com>
2020-02-28 19:11:19 +0300

    chore: bump workspace dependencies and fix build / test scripts

--- jest.config.snapshot.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  resolveSnapshotPath: (path, extension) => {
-    return `${path}${extension}`;
-  },
-  resolveTestPath: (path, extension) => {
-    return path.slice(0, -extension.length);
-  },
-  testPathForConsistencyCheck: 'module/testable.spec.ts'
-};
