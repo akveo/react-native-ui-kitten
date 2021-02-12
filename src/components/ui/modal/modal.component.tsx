15fb2854315057189797b45ecbd29422bee0336c
Ivan <whitestranger7@gmail.com>
2021-02-12 17:51:28 +0300

    fix(modal): add change orientation event with modal render (#1346)

--- src/components/ui/modal/modal.component.tsx
+++ src/components/ui/modal/modal.component.tsx
@@ -11,6 +11,7 @@ import {
   View,
   ViewProps,
   ViewStyle,
+  Dimensions,
 } from 'react-native';
 import {
   Frame,
@@ -94,6 +95,7 @@ export class Modal extends React.PureComponent<ModalProps, State> {
   };
 
   public componentDidMount(): void {
+    Dimensions.addEventListener('change', this.onDimensionChange);
     if (!this.modalId && this.props.visible) {
       this.show();
       return;
@@ -117,7 +119,14 @@ export class Modal extends React.PureComponent<ModalProps, State> {
   }
 
   public componentWillUnmount(): void {
+    Dimensions.removeEventListener('change', this.onDimensionChange);
     this.hide();
+  }
+
+  private onDimensionChange = (): void => {
+    if(this.props.visible) {
+      ModalService.update(this.modalId, this.renderMeasuringContentElement());
+    }
   }
 
   private onContentMeasure = (contentFrame: Frame): void => {
