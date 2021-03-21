ee6a3b19489361c6a418473c2b2a9b9ab2b460df
Ivan Vezhnavets <ivanvezhnavets@MacBook-Pro-Ivan.local>
2021-03-21 14:32:16 +0300

    bump: template packages

--- src/template-ts/template/App.tsx
+++ src/template-ts/template/App.tsx
@@ -11,10 +11,7 @@
  */
 
 import React from 'react';
-import {
-  ImageProps,
-  StyleSheet,
-} from 'react-native';
+import {ImageProps, StyleSheet} from 'react-native';
 import {
   ApplicationProvider,
   Button,
@@ -23,29 +20,29 @@ import {
   Layout,
   Text,
 } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 
 /**
  * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
  * https://akveo.github.io/eva-icons
  */
-const HeartIcon = (props?: Partial<ImageProps>): React.ReactElement<ImageProps> => (
-  <Icon {...props} name='heart'/>
-);
+ const HeartIcon = (
+  props?: Partial<ImageProps>,
+): React.ReactElement<ImageProps> => <Icon {...props} name="heart" />;
 
 export default (): React.ReactFragment => (
   <>
-    <IconRegistry icons={EvaIconsPack}/>
+    <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
       <Layout style={styles.container}>
-        <Text style={styles.text} category='h1'>
+        <Text style={styles.text} category="h1">
           Welcome to UI Kitten 😻
         </Text>
-        <Text style={styles.text} category='s1'>
+        <Text style={styles.text} category="s1">
           Start with editing App.js to configure your App
         </Text>
-        <Text style={styles.text} appearance='hint'>
+        <Text style={styles.text} appearance="hint">
           For example, try changing theme to Dark by using eva.dark
         </Text>
         <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
