6d37cce023ec47cc25d19a77bcd883aaf3ba9a27
Ivan Vezhnavets <ivanvezhnavets@MacBook-Pro-Ivan.local>
2021-03-31 18:07:42 +0300

    update: docs

--- src/components/ui/calendar/rangeCalendar.component.tsx
+++ src/components/ui/calendar/rangeCalendar.component.tsx
@@ -75,12 +75,12 @@ export type RangeCalendarElement<D = Date> = React.ReactElement<RangeCalendarPro
  * Called with a date for this cell and styles provided by Eva.
  * 
  * @property {(CalendarViewMode) => void} renderArrowLeft - Function component
- * to render instead of default left arrow.
- * viewMode returns string with current calendar view ("YEAR", "MONTH", "DATE").
+ * to render instead of default previous arrow.
+ * Accepts viewMode argument which returns string with current calendar view ("YEAR", "MONTH", "DATE").
  * 
  * @property {(CalendarViewMode) => void} renderArrowRight - Function component
- * to render instead of default right arrow.
- * viewMode returns string with current calendar view ("YEAR", "MONTH", "DATE").
+ * to render instead of default next arrow.
+ * Accepts viewMode argument which returns string with current calendar view ("YEAR", "MONTH", "DATE").
  * 
  * @property {(D, CalendarViewMode) => void} onVisibleDateChange - Called when navigating to the previous or next month / year.
  * viewMode returns string with current calendar view ("YEAR", "MONTH", "DATE").
