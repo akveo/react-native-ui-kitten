9f9ab67a01708266b6efd4fb05eca2661be94ea1
Ivan Vezhnavets <ivanvezhnavets@MacBook-Pro-Ivan.local>
2021-06-02 00:20:17 +0300

    Fix: format config

--- src/template-ts/template/metro.config.js
+++ src/template-ts/template/metro.config.js
@@ -5,22 +5,22 @@
  * @format
  */
 
- const MetroConfig = require('@ui-kitten/metro-config');
+const MetroConfig = require('@ui-kitten/metro-config');
 
- /**
-  * @see https://akveo.github.io/react-native-ui-kitten/docs/guides/improving-performance
-  */
- const evaConfig = {
-   evaPackage: '@eva-design/eva',
- };
- 
- module.exports = MetroConfig.create(evaConfig, {
-   transformer: {
-     getTransformOptions: async () => ({
-       transform: {
-         experimentalImportSupport: false,
-         inlineRequires: false,
-       },
-     }),
-   },
- });
+/**
+ * @see https://akveo.github.io/react-native-ui-kitten/docs/guides/improving-performance
+ */
+const evaConfig = {
+  evaPackage: '@eva-design/eva',
+};
+
+module.exports = MetroConfig.create(evaConfig, {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+});
