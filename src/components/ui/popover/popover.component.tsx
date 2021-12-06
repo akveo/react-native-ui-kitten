a2ac9cb07de7a163b8a359e9baa416b4c9f36947
Dmitry Vaytul <d.vaytul@gmail.com>
2021-12-06 11:16:48 +0300

    fix: block showing of popover after unmount

--- src/components/ui/popover/popover.component.tsx
+++ src/components/ui/popover/popover.component.tsx
@@ -9,7 +9,7 @@ import {
   StyleSheet,
   BackHandler,
   NativeEventSubscription,
-  Platform 
+  Platform,
 } from 'react-native';
 import {
   Frame,
@@ -60,7 +60,7 @@ interface State {
  * @method {() => void} hide - Sets `content` component invisible.
  *
  * @property {boolean} visible - Whether content component is visible.
- * Defaults to false.
+ * Defaults to false. Do not use at the same time as the show/hide methods.
  *
  * @property {() => ReactElement} anchor - A component relative to which content component will be shown.
  *
@@ -108,6 +108,7 @@ export class Popover extends React.Component<PopoverProps, State> {
   private modalId: string;
   private contentPosition: Point = Point.outscreen();
   private placementService: PopoverPlacementService = new PopoverPlacementService();
+  private isPopoverUnmounted: boolean;
 
   private actualPlacement: PopoverPlacement = this.preferredPlacement;
 
@@ -129,7 +130,9 @@ export class Popover extends React.Component<PopoverProps, State> {
   }
 
   public show = (): void => {
-    this.modalId = ModalService.show(this.renderMeasuringPopoverElement(), this.backdropConfig);
+    if (!this.isPopoverUnmounted) {
+      this.modalId = ModalService.show(this.renderMeasuringPopoverElement(), this.backdropConfig);
+    }
   };
 
   public hide = (): void => {
@@ -154,7 +157,7 @@ export class Popover extends React.Component<PopoverProps, State> {
   }
 
   public componentDidMount(): void {
-    if(Platform.OS === 'android') {
+    if (Platform.OS === 'android') {
       this.hardwareBackSubscription = BackHandler.addEventListener('hardwareBackPress', this.onHardwareBackPress);
     }
   }
@@ -162,6 +165,8 @@ export class Popover extends React.Component<PopoverProps, State> {
   public componentWillUnmount(): void {
     this.hardwareBackSubscription?.remove();
     this.hide();
+
+    this.isPopoverUnmounted = true;
   }
 
   private onChildMeasure = (childFrame: Frame): void => {
@@ -218,7 +223,7 @@ export class Popover extends React.Component<PopoverProps, State> {
 
   private renderMeasuringPopoverElement = (): MeasuringElement => {
     return (
-      <MeasureElement 
+      <MeasureElement
         shouldUseTopInsets={ModalService.getShouldUseTopInsets}
         onMeasure={this.onContentMeasure}>
           {this.renderPopoverElement()}
@@ -226,7 +231,7 @@ export class Popover extends React.Component<PopoverProps, State> {
     );
   };
 
-  public render(): React.ReactElement {    
+  public render(): React.ReactElement {
     return (
       <MeasureElement
         shouldUseTopInsets={ModalService.getShouldUseTopInsets}
