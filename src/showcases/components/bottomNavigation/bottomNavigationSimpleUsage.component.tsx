254f4514cec2b008afc2fb3c4289fcfecab50b78
Sergey Kozel <kozel.sergey@gmail.com>
2023-04-20 18:21:43 +0200

    fix: BottomNavigation vertical align [issue](https://github.com/akveo/react-native-ui-kitten/issues/1719) (#1721)

--- src/showcases/components/bottomNavigation/bottomNavigationSimpleUsage.component.tsx
+++ src/showcases/components/bottomNavigation/bottomNavigationSimpleUsage.component.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
-import { BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
+import { BottomNavigation, BottomNavigationTab, Text } from '@ui-kitten/components';
+import { StyleSheet, View } from 'react-native';
 
 export const BottomNavigationSimpleUsageShowcase = (): React.ReactElement => {
 
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   return (
-    <BottomNavigation
-      selectedIndex={selectedIndex}
-      onSelect={index => setSelectedIndex(index)}
-    >
-      <BottomNavigationTab title='USERS' />
-      <BottomNavigationTab title='ORDERS' />
-      <BottomNavigationTab title='TRANSACTIONS' />
-    </BottomNavigation>
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text
+          category='h4'
+          style={styles.text}
+        >
+          Content
+        </Text>
+      </View>
+      <BottomNavigation
+        selectedIndex={selectedIndex}
+        onSelect={index => setSelectedIndex(index)}
+      >
+        <BottomNavigationTab title='USERS' />
+        <BottomNavigationTab title='ORDERS' />
+        <BottomNavigationTab title='TRANSACTIONS' />
+      </BottomNavigation>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    backgroundColor: '#EEF1F6',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
