5a121fb302f85455b1c3143aa4c1f706d65de41b
Marko Mitrović <markomitrovic@Markos-MacBook-Pro.local>
2026-01-13 09:47:37 +0100

    draft update of component

--- src/components/ui/text/text.component.tsx
+++ src/components/ui/text/text.component.tsx
@@ -23,11 +23,15 @@ type TextStyledProps = Overwrite<StyledComponentProps, {
   appearance?: LiteralUnion<'default' | 'alternative' | 'hint'>;
 }>;
 
-type ChildElement = React.ReactText | TextElement;
-
 export interface TextProps extends RNTextProps, TextStyledProps {
-  children?: ChildElement | ChildElement[];
-  category?: LiteralUnion<'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 's1' | 's2' | 'p1' | 'p2' | 'c1' | 'c2' | 'label'>;
+  children?: React.ReactNode;
+  category?: LiteralUnion<
+    'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' |
+    's1' | 's2' |
+    'p1' | 'p2' |
+    'c1' | 'c2' |
+    'label'
+  >;
   status?: EvaStatus;
 }
 
